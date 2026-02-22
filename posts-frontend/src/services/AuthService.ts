239e349fcf952897c7732fc1ba6d1f2809080192
yaroslav <boliachevets.ya@gmail.com>
2026-02-22 08:27:21 +0000

    feat(auth): update router, navbar and login flow for new auth logic

--- posts-frontend/src/services/AuthService.ts
+++ posts-frontend/src/services/AuthService.ts
@@ -4,14 +4,14 @@ import { AuthResponce } from "../models/responce/AuthResponce";
 
 export const AuthService = {
 	login(email: string, password: string): Promise<AxiosResponse<AuthResponce>> {
-		return $api.post<AuthResponce>("/login", { email, password });
+		return $api.post<AuthResponce>("/api/user/login", { email, password });
 	},
 
 	registration(
 		email: string,
 		password: string,
 	): Promise<AxiosResponse<AuthResponce>> {
-		return $api.post<AuthResponce>("/registration", { email, password });
+		return $api.post<AuthResponce>("/api/user/registration", { email, password });
 	},
 
 	logout(): Promise<void> {
