7e0e8a2ca961d94167f687a7e338c69a7b93be38
yaroslav <boliachevets.ya@gmail.com>
2026-07-02 07:54:43 +0100

    fix(appRouter): add privatе-public routes and error navigation

--- posts-frontend/src/components/AppRouter.jsx
+++ posts-frontend/src/components/AppRouter.jsx
@@ -1,31 +1,29 @@
 import React, { useContext } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "../router/Routes.jsx";
-// import { AuthContext } from "../context/context.js";
 import { AuthContext } from "../models/AuthContextType";
 import Loader from "./UI/loader/Loader.jsx";
 import { Context } from "../main.tsx";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
+import Error from "../pages/Error.jsx";
 
 function AppRouter() {
 	// берем из context значение глобальной переменной
-	// const { isAuth, isLoading } = useContext(AuthContext);
 	const { store } = useContext(Context);
-	const navigate = useNavigate();
+	const { setGuest, checkAuth } = store;
+
 	useEffect(() => {
 		if (localStorage.getItem("guest") === "true") {
-			store.setGuest(true);
+			setGuest(true);
 
 			return;
 		}
 
 		if (localStorage.getItem("token")) {
-			store.checkAuth();
-		}
-
-		if (store.isGuest == false && !localStorage.getItem("token")) {
-			navigate("/login");
+			checkAuth();
+		} else {
+			store.setIsLoading(false);
 		}
 	}, []);
 
@@ -33,28 +31,27 @@ function AppRouter() {
 		return <Loader />;
 	}
 
-	// ================================================================
+	const hasAccess = store.isAuth || store.isGuest;
+
 	// в зависимости от переменной isAuth будут доступны приватные или публичные пути, в публичном только авторизация
-	// return store.isAuth ? (
-	// 	<Routes>
-	// 		{privateRoutes.map(({ path, element }) => {
-	// 			return <Route key={path} path={path} element={element} />;
-	// 		})}
-	// 	</Routes>
-	// ) : (
-	// 	<Routes>
-	// 		{publicRoutes.map(({ path, element }) => {
-	// 			return <Route key={path} path={path} element={element} />;
-	// 		})}
-	// 	</Routes>
-	// );
-	// ==========================================================
 
 	return (
 		<Routes>
-			{privateRoutes.map(({ path, element }) => {
-				return <Route key={path} path={path} element={element} />;
-			})}
+			{hasAccess ? (
+				<>
+					{privateRoutes.map(({ path, element }) => {
+						return <Route key={path} path={path} element={element} />;
+					})}
+					<Route path="*" element={<Error />}></Route>
+				</>
+			) : (
+				<>
+					{publicRoutes.map(({ path, element }) => {
+						return <Route key={path} path={path} element={element} />;
+					})}
+					<Route path="*" element={<Navigate to="/login" replace />}></Route>
+				</>
+			)}
 		</Routes>
 	);
 }
