d33a897276520552ce40790749f6a5b69b47c24e
yaroslav <boliachevets.ya@gmail.com>
2026-07-03 23:37:48 +0100

    refactor(frontend): clean up unused imports, implenemt routpath to nav

---
+++ posts-frontend/src/router/AppRouter.tsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { privateRoutes, publicRoutes, RoutePath } from "./Routes.tsx";
+import { Context } from "../main.tsx";
+import { observer } from "mobx-react-lite";
+import Error from "../pages/Error.jsx";
+import Loader from "../components/UI/loader/Loader.jsx";
+
+function AppRouter() {
+	// берем из context значение глобальной переменной
+	const { store } = useContext(Context);
+	const { setGuest, checkAuth } = store;
+
+	useEffect(() => {
+		if (localStorage.getItem("guest") === "true") {
+			setGuest(true);
+
+			return;
+		}
+
+		if (localStorage.getItem("token")) {
+			checkAuth();
+		} else {
+			store.setIsLoading(false);
+		}
+	}, [store]);
+
+	if (store.isLoading) {
+		return <Loader />;
+	}
+
+	const hasAccess = store.isAuth || store.isGuest;
+
+	// в зависимости от переменной isAuth будут доступны приватные или публичные пути, в публичном только авторизация
+
+	return (
+		<Routes>
+			{hasAccess ? (
+				<>
+					{privateRoutes.map(({ path, element }) => {
+						return <Route key={path} path={path} element={element} />;
+					})}
+					<Route path="*" element={<Error />}></Route>
+				</>
+			) : (
+				<>
+					{publicRoutes.map(({ path, element }) => {
+						return <Route key={path} path={path} element={element} />;
+					})}
+					<Route
+						path="*"
+						element={<Navigate to={RoutePath.LOGIN} replace />}
+					></Route>
+				</>
+			)}
+		</Routes>
+	);
+}
+
+export default observer(AppRouter);
