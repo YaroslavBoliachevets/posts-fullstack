d1b8f55ae309e7b804cb510b1f3b57b8014a4773
yaroslav <boliachevets.ya@gmail.com>
2026-07-10 18:00:44 +0100

    refactor(loader): migrate Loader to ts

---
+++ posts-frontend/src/components/UI/loader/Loader.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+import classes from "./Loader.module.css";
+
+const Loader: FC = () => {
+	return <div className={classes.loader}></div>;
+};
+export default Loader;
